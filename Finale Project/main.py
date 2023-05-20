bb3853b8ef2699ac1b3bbe037bcb7a48f5bee9fa
24rcarter <126002276+24rcarter@users.noreply.github.com>
2023-05-19 19:55:16 -0600

    main.py

--- Finale Project/main.py
+++ Finale Project/main.py
@@ -1,21 +1,21 @@
 import pygame
 import random
-import time
 
-screen = pygame.display.set_mode((640,480))
-pygame.display.set_caption("Side Scroller")
+screen = pygame.display.set_mode((640,480)) #creates window 
+pygame.display.set_caption("Side Scroller") #names window 
 
 def menu():
+    #displays menu
     image = pygame.image.load('assets\menu.png')
     image = pygame.transform.scale(image, (640,480))
     while True:
-        screen.blit(image, (0,0))
+        screen.blit(image, (0,0)) #displays menu inside of window
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.display.quit()
                 exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN: #adds continue if mouse is clicked in window
                 if event.pos[0] in range(300, 325) and event.pos[1] in range(200, 228):
                     game()
 menu()
@@ -39,6 +39,12 @@ def game():
     crate = pygame.transform.rotozoom(crate, 0, 0.8)
     crate_x = 700
     crate_speed = 2
+
+    #make font and score variables
+    font = pygame.font.SysFont('comicsans', 80)
+    score_counter = 0
+    prev_score = 0
+    new_score = 0
     
     while True:
         #create background, allows for smooth 
@@ -46,11 +52,15 @@ def game():
         screen.blit(image, (0,0))
         screen.blit(image, (bgx + 640,0))
 
+        #moves background across screan
         bgx = bgx - 1
         if bgx <= -640:
             bgx = 0
 
-        p_coll_box = screen.blit(player, (50, 325))
+        
+        p_coll_box = screen.blit(player, (50, 325)) #creates a hit detection box around the player
+        
+        #adds the jump mechanic, adds a jump peak
         if player_j < 325:
             player_j += gravity
         if jump == 1:
@@ -60,26 +70,43 @@ def game():
                 jump_count = 0
                 jump = 0
 
-        c_coll_box= screen.blit(crate,(crate_x, 360))
-        crate_x -= crate_speed
+        c_coll_box = screen.blit(crate,(crate_x, 360)) #creates hit detection for box
+        crate_x -= crate_speed #moves the crate left by the speed 
         if crate_x < - 50:
+            #randomly places a box and it's speed, creating variation
             crate_x == random.randint(700, 800)
             crate_speed == random.randint(2, 5)
+            score_counter += 1
+        
 
-        if p_coll_box.colliderect(c_coll_box):
-            crate_speed = 0
-            player = pygame.transform.rotozoom(player, 90, 0)
-            time.sleep(4)
+        if p_coll_box.colliderect(c_coll_box): #if player and crate collide
+            crate_speed = 0 #crate stops
+            player = pygame.transform.rotozoom(player, 90, 0) #player falls
+            pygame.time.delay(100) #delay game over screen
+            
+            #will display (hopefully) the game over screen (hopefully) over the background
             end = pygame.image.load('assets\gameover.png')
             screen.blit(end, (0, 0))
             
+            #will display (hopefully) the score the player got
             pygame.display.update()
             for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.display.quit()
+                    exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    print('scores')
+                    #display the highest score on screen
+                    prev_score = font.render('Previous Score: ' + str(update_file()), 1, (255, 255, 255))
+                    screen.blit(prev_score, (320 - prev_score.get_width()/2, 240))
+                    
+                    #display the current score on screen
+                    new_score = font.render('Score: ' + str(score_counter), 1, (255, 255, 255))
+                    screen.blit(new_score, (320 - new_score.get_width()/2, 320)) 
+                    
+                    pygame.display.update() #update screen         
             
-            
-            pygame.display.update()
+            #if game is over, will send player back to menu after a mouse click
+            pygame.display.update(100)
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     return menu()
@@ -89,7 +116,22 @@ def game():
             if event.type == pygame.QUIT:
                 pygame.display.quit()
                 exit()
-            if event.type == pygame.KEYDOWN:   
+            if event.type == pygame.KEYDOWN and jump != 1: #will (hopefully) make it so the player can jump, but only once
                 jump = 1
 menu()
 
+#uses score.txt to store the highest score
+def update_file(score_counter):
+    f = open('scores.txt', 'r')
+    file = f.readlines()
+    last = int(file[0])
+
+    if last < int(score_counter): #if the highest score is less then current score
+        f.close()
+        file = open('scores.txt', 'w')
+        file.write(str(score_counter))
+        file.close()
+
+        return score_counter
+    f.close()
+    return last
